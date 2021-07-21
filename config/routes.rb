e464ea62b3363fcff3b350e8ff585346f5282ed0
Emily <emily.elim04@gmail.com>
2021-07-21 15:59:59 -0400

    add articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
-Rails
-  .application
-  .routes
-  .draw do
+Rails.application.routes.draw do
     root 'welcome#home'
     get 'about', to: 'welcome#about'
+
+    resources :articles, only: [:show]
   end
