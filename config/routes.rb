570f3286949de824aab5ffed72347a5d23beee19
Emily <emily.elim04@gmail.com>
2021-07-22 10:34:17 -0400

    add error msg for validation of form and flash msg when form is successfully completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
     root 'welcome#home'
     get 'about', to: 'welcome#about'
 
-    resources :articles, only: [:show, :index]
+    resources :articles, only: [:show, :index, :new, :create]
   end
