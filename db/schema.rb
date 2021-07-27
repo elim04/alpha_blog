32f75f7ec5420d68ea04abeb76b6765cd95f8f4b
Emily <emily.elim04@gmail.com>
2021-07-27 16:53:43 -0400

    add bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210727181519) do
+ActiveRecord::Schema.define(version: 20210727203754) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20210727181519) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
