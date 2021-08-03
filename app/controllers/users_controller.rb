f6b4df8f2fdf31ff5ca3d66e24d5b0ef99aeb3a0
Emily <emily.elim04@gmail.com>
2021-08-03 16:13:58 -0400

    add delete functionality and admin column to schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def show 
-    @user = User.find(params[:id]) 
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -14,11 +16,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update 
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account was updated!!"
       redirect_to @user
@@ -32,11 +32,19 @@ class UsersController < ApplicationController
     @user = User.new(user_params) 
 
     if @user.save
+      session[:user_id] = @user.id 
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have signed up!"
       redirect_to articles_path
     else
       render "new"
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
   end
 
   private 
@@ -45,4 +53,14 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account silly!"
+      redirect_to @user
+    end
+  end
 end
